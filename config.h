1a09eb885afc624136cc66b0304628bd8c19a647
Ollilauch <oliverjbmub@gmail.com>
2024-09-04 03:30:55 +0200

    changed term to alacritty; changed MODKEY to windows key

--- config.h
+++ config.h
@@ -68,7 +68,7 @@ static const int repeat_delay = 600;
 static const int tap_to_click = 1;
 static const int tap_and_drag = 1;
 static const int drag_lock = 1;
-static const int natural_scrolling = 0;
+static const int natural_scrolling = 1;
 static const int disable_while_typing = 1;
 static const int left_handed = 0;
 static const int middle_button_emulation = 0;
@@ -108,7 +108,7 @@ LIBINPUT_CONFIG_TAP_MAP_LMR -- 1/2/3 finger tap maps to left/middle/right
 static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TAP_MAP_LRM;
 
 /* If you want to use the windows key for MODKEY, use WLR_MODIFIER_LOGO */
-#define MODKEY WLR_MODIFIER_ALT
+#define MODKEY WLR_MODIFIER_LOGO
 
 #define TAGKEYS(KEY,SKEY,TAG) \
 	{ MODKEY,                    KEY,            view,            {.ui = 1 << TAG} }, \
@@ -120,7 +120,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *termcmd[] = { "foot", NULL };
+static const char *termcmd[] = { "alacritty", NULL };
 static const char *menucmd[] = { "wmenu-run", NULL };
 
 static const Key keys[] = {
@@ -134,7 +134,9 @@ static const Key keys[] = {
     { MODKEY,                    XKB_KEY_d,          incnmaster,     {.i = -1} },
     { MODKEY,                    XKB_KEY_h,          setmfact,       {.f = -0.05f} },
     { MODKEY,                    XKB_KEY_l,          setmfact,       {.f = +0.05f} },
-    { MODKEY,                    XKB_KEY_Return,     zoom,           {0} },
+    {
+        MODKEY,                    XKB_KEY_Return,     zoom,           {0}
+    },
     { MODKEY,                    XKB_KEY_Tab,        view,           {0} },
     { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_C,          killclient,     {0} },
     { MODKEY,                    XKB_KEY_t,          setlayout,      {.v = &layouts[0]} },
